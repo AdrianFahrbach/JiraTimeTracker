a346accd9d3efcea94260e781505f4e39b432320
Adrian Fahrbach <adrian@hatchery.io>
2024-02-26 23:53:57 +0100

    ✨ feat: test windows menu

--- App.tsx
+++ App.tsx
@@ -1,26 +1,30 @@
 import { Provider } from 'jotai';
 import React, { FC, Suspense } from 'react';
-import { Text } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { store } from './src/atoms';
 import { ColorSchemeWatcher } from './src/components/ColorSchemeWatcher';
 import { DebugTools } from './src/components/DebugTools';
 import { WorklogStateWatcher } from './src/components/WorklogStateWatcher';
 import { GlobalProvider } from './src/providers/GlobalProvider';
 import { Main } from './src/screens/Main';
+import { Menu, MenuTrigger, MenuPopover, MenuList, MenuItem } from '@fluentui-react-native/menu';
 
 const App: FC = () => {
   return (
-    // TODO @AdrianFahrbach make pretty
-    <Suspense fallback={<Text>Loading...</Text>}>
-      <Provider store={store}>
-        <WorklogStateWatcher />
-        <ColorSchemeWatcher />
-        <GlobalProvider>
-          <Main />
-          {__DEV__ && <DebugTools />}
-        </GlobalProvider>
-      </Provider>
-    </Suspense>
+    <View style={{ marginTop: 100 }}>
+      <Menu>
+        <MenuTrigger>
+          <Button title='Open Menu' />
+        </MenuTrigger>
+        <MenuPopover>
+          <MenuList>
+            <MenuItem>Option 1</MenuItem>
+            <MenuItem>Option 2</MenuItem>
+            <MenuItem>Option 3</MenuItem>
+          </MenuList>
+        </MenuPopover>
+      </Menu>
+    </View>
   );
 };
 
